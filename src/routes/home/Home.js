646239f3f62ce8d644ba44fdcc6ad6747e9447e1
Seven4ME <nyanjasiek@gmail.com>
2018-12-10 21:44:47 +0200

    calculator section

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,47 +1,110 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
 class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        link: PropTypes.string.isRequired,
-        content: PropTypes.string,
-      }),
-    ).isRequired,
-  };
-
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>React.js News</h1>
-          {this.props.news.map(item => (
-            <article key={item.link} className={s.newsItem}>
-              <h1 className={s.newsTitle}>
-                <a href={item.link}>{item.title}</a>
-              </h1>
-              <div
-                className={s.newsDesc}
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{ __html: item.content }}
-              />
-            </article>
-          ))}
+      <section className={s.banner}>
+        <div className={s.root}>
+          <div className={s.container}>
+            <div className="row">
+              <div className="col-xs-12 col-sm-12 col-md-12 ">
+                <div className="container">
+                  <div className={s.banner__heading}>
+                    <h1>
+                      Получите до 3000 грн со 100% * скидкой на свой первый
+                      кредит!
+                    </h1>
+                  </div>
+                  <div className={s.banner__calc}>
+                    <div className="row center-md">
+                      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                        <div className={s.calculator__left}>
+                          <div className={s.calculator__left_heading}>
+                            <span>Получить кредит за 10 минут!</span>
+                          </div>
+                          <div className={s.calculator__left_wrapper}>
+                            <div className={s.loan__ammount}>
+                              <div className={s.ammount__values}>
+                                <p>Сумма</p>
+                                <span id="money">2150 грн.</span>
+                              </div>
+                              <div className={s.ammount__container}>
+                                  
+                              </div>
+                              </div>
+                            <div className={s.term}>
+                              <div className={s.term__days}>
+                                <div className={s.days__value}>
+                                    <p>Срок</p>
+                                    <span id="days">14 дн.</span>
+                                </div>
+                                
+                                <div className={s.days__container}>
+                                
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                        <div className={s.calculator__right}>
+                          <div className={s.calculator__right_heading}>
+                            <span>Общая сумма к оплате</span>
+                          </div>
+                          <div className={s.total}>
+                            <div className={s.total__wrapper}>
+                              <table>
+                                <tbody>
+                                  <tr>
+                                    <td>Кредит:</td>
+                                    <td className="credit">
+                                      <div id="rangeValue">0</div>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Проценты:</td>
+                                    <td className="credit" id="percentage">
+                                      2
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Всего:</td>
+                                    <td className="credit" id="fullCredit">
+                                      2
+
+                                      </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Дата погашения:</td>
+                                    <td className="credit" id="dateEnd">
+                                      2
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                            <a className={s.total__button} href="#">
+                              Подать заявку
+                            </a>
+                            <div className={s.round}>
+                              <input type="checkbox" id="checkbox" />
+                              <label htmlFor="checkbox"></label>
+                              <p className={s.check_deskription}>Я новый клиент</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
